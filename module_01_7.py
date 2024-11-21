ea10643d3da7af2dce0a4101f189a0db3a1f4143
sergeuts <92944373+sergeuts@users.noreply.github.com>
2024-11-21 22:07:31 +0300

    Add files via upload

---
+++ module_01_7.py
@@ -0,0 +1,4 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+mean_grades = [sum(g)/len(g) for g in grades]
+print(dict(zip(sorted(students), mean_grades)))
