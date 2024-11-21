c7f4e4b112569f42a44954894434a29173dbda0d
sergeuts <92944373+sergeuts@users.noreply.github.com>
2024-11-21 19:46:18 +0300

    Add files via upload

---
+++ 01_2_hw.py
@@ -0,0 +1,5 @@
+tasks = 12
+time_spent = 1.5
+course = 'Python'
+time_for_1_task = time_spent / tasks
+print(f'Курс: Python, всего задач: {tasks}, затрачено часов: {time_spent}, среднее время выполнения: {time_for_1_task} часа.')
